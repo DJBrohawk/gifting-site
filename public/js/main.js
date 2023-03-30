5f213d9c699718c82965caa8ad5b426c0528c0aa
jeramee <jerameeoliver@gmail.com>
2023-03-29 23:39:42 -0500

    Major changes to file structure. Config file changes, and path changes, minor JS, added 2 favicon images.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,38 @@
+// Language: JavaScript
 
+//import express from "express";
+// import bodyParser from "body-parser"; // dont think I need this
+
+//const express = require('express');
+//const bodyParser = require('body-parser');
+
+//const main = express();
+//const port = 5000;
+
+// Static Files
+//use('/', this.static('public'));
+//use('/css', this.static(__dirname + 'public/css'));
+//use('/img', this.static(__dirname + 'public/img'));
+//use('/js', this.static(__dirname + 'public/js'));
+
+// main.use(express.static(__dirname + 'public/css'));
+// main.use('/img', express.static(__dirname + 'public/img'));
+// main.use('/js', express.static(__dirname + 'public/js')); 
+
+// Templating Engine
+// app.set('views', './src/views')
+// app.set('view engine', 'ejs')
+
+//main.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+// const newsRouter = require('./src/routes/news')
+
+// app.use('/', newsRouter)
+// app.use('/article', newsRouter)
+
+// Listen on port 5000
+//main.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
 
 
 
